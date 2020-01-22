ee5de94ec9d6c160a2e6746efd92c70bfb8c0230
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-01-22 14:38:25 -0600

    setup redux and got components connected

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
@@ -7,14 +6,6 @@ import Total from './components/Total';
 
 const App = () => {
   const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
     additionalFeatures: [
       { id: 1, name: 'V-6 engine', price: 1500 },
       { id: 2, name: 'Racing detail package', price: 1500 },
